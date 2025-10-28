dbdd0f9ba4b09da685a304393d04c295ccf1319d
arseniy <teleblueberry@gmail.com>
2025-10-28 03:41:28 +0300

    initial commit

---
+++ extension.js
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "gitpush-button" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('gitpush-button', function () {
+		// The code you place here will be executed every time your command is executed
+		vscode.window.showInformationMessage('Git push started!');
+
+		let terminal = vscode.window.activeTerminal;
+
+		terminal.sendText(`git add -A`);
+
+		setTimeout(() => {
+                terminal.sendText(`git commit -m "Auto Commit"`);
+            }, 500);
+            
+            setTimeout(() => {
+                terminal.sendText(`git push -u origin HEAD`);
+                vscode.window.showInformationMessage('Git push completed!');
+            }, 1000);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
